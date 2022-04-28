4b9486b9f3e7f725d2cfae91648f24a72714b2e3
eric220 <criteser2@gmail.com>
2022-04-28 13:57:22 -0500

    correct map name in main

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ gdf = gpd.read_file("deploy_data/gdf_w_clusters.shp")
 
 @app.route('/')
 def home():
+    #may be able to use chaining
     folium_map = gdf.explore(column = 'rel_probs', cmap='seismic', legend_kwds = {'caption': 'Realtive Probability'},\
                    prefer_canvas = True)
     return folium_map._repr_html_()
